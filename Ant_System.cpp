7d7f079851277a1b6b5c485c5650b5c4f31c920d
Giovanni brunetti <gb1609@outlook.com>
2019-07-15 14:10:10 +0200

    Add adjacents

--- Ant_System.cpp
+++ Ant_System.cpp
@@ -3,18 +3,18 @@ using namespace std;
 #include "Utilities/Graph.h"
 #include <math.h>
 
-void
-initializateG (Graph g)
-{
-  for (int a = 0; a < g.getDimMax (); a++)
-    for (int b = 0; b < g.getDimMax (); b++)
-      if (g.isNear (a, b)) g.setAdjacents(a,b);
+void initializateG(Graph g) {
+	for (int a = 0; a < g.getDimMax(); a++)
+		for (int b = 0; b < g.getDimMax(); b++)
+			if (g.isNear(a, b))
+				cout << a << " e " << b << " sono vicini" << endl;
+//			else
+//				cout << a << " e " << b << " non sono vicini" << endl;
+//				g.setAdjacents(a, b);
 }
 
-int
-main (int argc, char **argv)
-{
-  Graph g (5);
-  initializateG (g);
-  return 0;
+int main(int argc, char **argv) {
+	Graph g(5);
+	initializateG(g);
+	return 0;
 }
--- Utilities/Graph.h
+++ Utilities/Graph.h
@@ -14,130 +14,149 @@ using namespace std;
 #include <utility>
 #include "Ant.h"
 
-struct Cell
-{
-    int ant_size;
-    vector<Ant> ants;
-    vector<int> directions;
-    int pheromone;
-    bool source;
-    bool food;
+struct Cell {
+	int ant_size;
+	vector<Ant> ants;
+	vector<int> directions;
+	int pheromone;
+	bool source;
+	bool food;
 };
 
-class Graph
-{
-  private:
-    bool **adjacents;
-    Cell **cells;
-    int dim;
-    int max_num;
+class Graph {
+private:
+	bool **adjacents;
+	Cell **cells;
+	int dim;
+	int max_num;
 
-  public:
-    Graph (int w)
-    {
-      dim = w;
-      max_num = dim * dim;
-      adjacents = (bool **) malloc (max_num * sizeof(bool*));
-      cells = (Cell**) malloc (max_num * sizeof(Cell*));
-      for (int i = 0; i < dim; i++)
-      {
-	adjacents[i] = (bool *) malloc (max_num * sizeof(bool));
-	cells[i] = (Cell *) malloc (max_num * sizeof(Cell));
-      }
-    }
-    void
-    addCell ()
-    {
+public:
+	Graph(int w) {
+		dim = w;
+		max_num = dim * dim;
+		adjacents = (bool**) malloc(dim * sizeof(bool*));
+		cells = (Cell**) malloc(dim * sizeof(Cell*));
+		for (int i = 0; i < dim; i++) {
+			adjacents[i] = (bool*) malloc(dim * sizeof(bool));
+			cells[i] = (Cell*) malloc(dim * sizeof(Cell));
+		}
+	}
 
-    }
-    
-    bool**
-    getAdjacents () const
-    {
-      return adjacents;
-    }
-    
-    void
-    setAdjacents (int a, int b) const
-    {
-      adjacents[a][b] = true;
-    }
+	bool**
+	getAdjacents() const {
+		return adjacents;
+	}
 
-    int
-    getDim ()
-    {
-      return dim;
-    }
+	void setAdjacents(int a, int b) const {
+		adjacents[a][b] = true;
+	}
 
-    int
-    getDimMax ()
-    {
-      return max_num;
-    }
+	int getDim() {
+		return dim;
+	}
 
-    void
-    printGAdj ()
-    {
-      for (int a = 0; a < dim; a++)
-      {
-	for (int b = 0; b < dim; b++)
-	  cout << adjacents[a][b] << " - ";
-	cout << endl;
-      }
-    }
+	int getDimMax() {
+		return max_num;
+	}
 
-    int
-    pairToInt (int row, int col)
-    {
-      return row * dim + col;
-    }
+	void printGAdj() {
+		for (int a = 0; a < dim; a++) {
+			for (int b = 0; b < dim; b++)
+				cout << adjacents[a][b] << " - ";
+			cout << endl;
+		}
+	}
 
-    pair<int, int>
-    intToPair (int num)
-    {
-      pair<int, int> p;
-      int x = num / dim;
-      int y = num % dim;
-      p = make_pair (x, y);
-      return p;
-    }
-    int
-    left (int n)
-    {
-      if (n - 1 > -1) return n - 1;
-      return -1;
-    }
+	int pairToInt(int row, int col) {
+		return row * dim + col;
+	}
 
-    int
-    right (int n)
-    {
-      if (n + 1 < max_num) return n + 1;
-      return -1;
-    }
+	pair<int, int> intToPair(int num) {
+		pair<int, int> p;
+		int x = num / dim;
+		int y = num % dim;
+		p = make_pair(x, y);
+		return p;
+	}
+	int left(int n) {
+		int toCheck = n - 1;
+		if (toCheck > -1 && toCheck % dim != dim - 1)
+			return n - 1;
+		return -1;
+	}
 
-    int
-    down (int n)
-    {
-      if (n + dim < max_num) return n + 1;
-      return -1;
-    }
+	int right(int n) {
+		int toCheck = n + 1;
+		if (toCheck < max_num && toCheck % dim != 0)
+			return n + 1;
+		return -1;
+	}
 
-    int
-    up (int n)
-    {
-      if (n - dim >= 0) return n + 1;
-      return -1;
-    }
+	int down(int n) {
+		if (n + dim < max_num)
+			return n + dim;
+		return -1;
+	}
 
-    bool
-    isNear (int a, int b)
-    {
-      int l = left (a);
-      int r = right (a);
-      int u = up (a);
-      int d = down (a);
-      return b == l || b == r || b == u || b == d;
-    }
+	int up(int n) {
+		if (n - dim > -1)
+			return n - dim;
+		return -1;
+	}
+
+	int up_left(int n) {
+		int toCheck = up(n) - 1;
+		if (toCheck > -1 && (toCheck % dim) != dim - 1)
+			return toCheck;
+		return -1;
+	}
+
+	int up_right(int n) {
+		int toCheck = up(n) + 1;
+		if (toCheck > -1 && (toCheck % dim) != 0)
+			return toCheck;
+		return -1;
+	}
+
+	int down_right(int n) {
+		int toCheck = down(n) + 1;
+		if (toCheck < max_num && (toCheck % dim) != 0)
+			return toCheck;
+		return -1;
+	}
+
+	int down_left(int n) {
+		int toCheck = down(n) - 1;
+		if (toCheck < max_num && (toCheck % dim) != dim - 1)
+			return toCheck;
+		return -1;
+	}
+
+	bool isNear(int a, int b) {
+		if (a == b)
+			return false;
+		int l = left(a);
+//		cout << "LEFT:" << l << endl;
+		int r = right(a);
+//		cout << "RIGHT:" << r << endl;
+		int u = up(a);
+//		cout << "UP:" << u << endl;
+		int d = down(a);
+//		cout << "DOWN:" << d << endl;
+		int ul = up_left(a);
+//		cout << "UP-LEFT:" << ul << endl;
+		int ur = up_right(a);
+//		cout << "UP_RIGHT:" << ur << endl;
+		int dl = down_left(a);
+//		cout << "DOWN_LEFT:" << dl << endl;
+		int dr = down_right(a);
+//		cout << "DOWN_RIGHT:" << dr << endl;
+		return b == l || b == r || b == u || b == d || b == ul || b == ur
+				|| b == dl || b == dr;
+	}
+	void pheromone_update() {
+
+	}
 };
 
 #endif /* GRAPH_H_ */
